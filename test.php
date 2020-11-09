b27ecabcedaa9008e70b311cfb8e05ca6be6bfdb
Gordon <8721141+SirDagen@users.noreply.github.com>
2020-11-09 21:53:31 +0100

    Update test.php

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ $GLOBALS['zdata']=array(
 	'title'=>'' /*can be empty*/ 
 	,'h1'=>'Test' // <a href='./img/AKT_5295_G1_2b.jpg'><img src='./img/AKT_5295_G1_2b-198x250.jpg' alt='' height='250' class='alignleft size-medium' /></a>
 	,'body'=>"<p>Testseite...</p>
-<p><a href='kontakt.php'>Kontakt ></a></p>"
+<p><a href='{$GLOBALS['zlink']['kontakt']}'>Kontakt ></a></p>"
 	);
 
 
